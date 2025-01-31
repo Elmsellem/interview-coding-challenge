5ddbdb4e60988f510b7a85659381e070fd2c1dae
Mohammed El Msellem <elmsellem102@gmail.com>
2025-01-31 22:46:32 +0100

    Unit tests for CommissionCalculatorService service

---
+++ tests/Unit/Services/Commission/CommissionCalculatorServiceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Elmsellem\Tests\Unit\Services\Commission;
+
+use Elmsellem\Enums\{ClientType, Currency, OperationType};
+use Elmsellem\Models\Operation;
+use Elmsellem\Services\{Commission\CommissionCalculatorService,
+    Commission\CommissionHandler,
+    Commission\CommissionRulesRegistry};
+use Mockery;
+use PHPUnit\Framework\{Attributes\After, Attributes\Before, TestCase};
+
+class CommissionCalculatorServiceTest extends TestCase
+{
+    private int $currentBcScale;
+
+    #[Before]
+    protected function saveBcScale(): void
+    {
+        $this->currentBcScale = bcscale();
+    }
+
+    #[After]
+    protected function revertBcScale(): void
+    {
+        bcscale($this->currentBcScale);
+    }
+
+    public function testCalculateCommission(): void
+    {
+        bcscale(10);
+        $registryMock = Mockery::mock(CommissionRulesRegistry::class);
+        $commissionHandler = Mockery::mock(CommissionHandler::class);
+        $calculator = new CommissionCalculatorService($registryMock);
+
+        $operation = new Operation();
+        $operation->setOperationType(OperationType::DEPOSIT);
+        $operation->setClientType(ClientType::PRIVATE);
+        $operation->setCurrency(Currency::from('EUR'));
+
+        $registryMock->shouldReceive('getCommissionHandler')
+            ->once()
+            ->with($operation->getOperationType(), $operation->getClientType())
+            ->andReturn($commissionHandler);
+
+        $commissionHandler->shouldReceive('calculate')
+            ->once()
+            ->with($operation)
+            ->andReturn('0.023');
+
+        $result = $calculator->calculateCommission($operation);
+
+        $this->assertEquals('0.03', $result);
+    }
+}
