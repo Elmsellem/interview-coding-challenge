5bc018df110f84fa4e66bbd15c37fc0938b2e21a
Mohammed El Msellem <elmsellem102@gmail.com>
2025-02-01 16:02:28 +0100

    Unit tests for ExchangeRatesService service

---
+++ tests/Unit/Services/ExchangeRatesServiceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Elmsellem\Tests\Unit\Services;
+
+use Elmsellem\DTOs\ExchangeRatesDTO;
+use Elmsellem\Services\ExchangeRatesService;
+use Elmsellem\Tests\ReflectionHelper;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use ReflectionException;
+
+class ExchangeRatesServiceTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    protected ExchangeRatesService $service;
+    protected Client $mockClient;
+
+    /**
+     * @throws ReflectionException
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockClient = Mockery::mock(Client::class);
+        $this->service = Mockery::mock(ExchangeRatesService::class)->makePartial();
+
+        ReflectionHelper::setProtectedProperty($this->service, 'client', $this->mockClient);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function testGetExchangeRates(): void
+    {
+        $responseMock = Mockery::mock(ResponseInterface::class);
+        $responseMock->shouldReceive('getBody->getContents')->andReturn(json_encode([
+            'base' => 'EUR',
+            'rates' => ['USD' => 1.18, 'JPY' => 130.15],
+            'timestamp' => 1627654321,
+            'date' => '2022-07-30'
+        ]));
+
+        $this->mockClient->shouldReceive('get')->andReturn($responseMock);
+
+        $rates = $this->service->getExchangeRates();
+
+        $this->assertInstanceOf(ExchangeRatesDTO::class, $rates);
+        $this->assertEquals('EUR', $rates->base);
+        $this->assertArrayHasKey('USD', $rates->rates);
+        $this->assertEquals(1.18, $rates->rates['USD']);
+    }
+
+    /**
+     * @throws ReflectionException
+     * @throws GuzzleException
+     */
+    public function testGetExchangeRatesWithCache(): void
+    {
+        $cachedRates = new ExchangeRatesDTO(
+            1627654321,
+            'EUR',
+            '2022-07-30',
+            ['USD' => 1.18, 'JPY' => 130.15],
+        );
+
+        ReflectionHelper::setProtectedProperty($this->service, 'cache', $cachedRates);
+        $rates = $this->service->getExchangeRates();
+
+        $this->assertSame($cachedRates, $rates);
+    }
+}
